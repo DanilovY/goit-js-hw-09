22f3d492fb01d97c2c181980cd41733904f8e044
Yurii Danilov <danilovyuriiii@gmail.com>
2024-11-24 10:29:16 +0300

    add

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,2 +1,55 @@
 'use strict';
-console.log('form');
+const storageKey = 'feedback-form-state';
+
+const formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.elements.email;
+const messageInput = form.elements.message;
+
+function saveToLocalStorage() {
+  localStorage.setItem(storageKey, JSON.stringify(formData));
+}
+
+function loadFromLocalStorage() {
+  const savedData = localStorage.getItem(storageKey);
+
+  if (savedData) {
+    const parsedData = JSON.parse(savedData);
+
+    formData.email = parsedData.email || '';
+    formData.message = parsedData.message || '';
+
+    emailInput.value = formData.email;
+    messageInput.value = formData.message;
+  }
+}
+
+form.addEventListener('input', event => {
+  formData[event.target.name] = event.target.value;
+
+  saveToLocalStorage();
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (!formData.email.trim() || !formData.message.trim()) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log('Submitted data:', formData);
+
+  localStorage.removeItem(storageKey);
+
+  formData.email = '';
+  formData.message = '';
+
+  form.reset();
+});
+
+loadFromLocalStorage();
